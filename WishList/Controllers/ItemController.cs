c552c6fd0af04b2ece8c83374be22a41c3f24705
Balonich <balonich@gmail.com>
2021-04-23 20:38:46 +0300

    Changed the method from SingleOrDefault to FirstOrDefault

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -36,7 +36,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            var item = _context.Items.SingleOrDefault(i => i.Id == Id);
+            var item = _context.Items.FirstOrDefault(i => i.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
 
